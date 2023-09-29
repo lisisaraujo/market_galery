531f63723c277a9ad673d8c8b5afdbf320979f83
Lisis <laruschel@proton.me>
2023-09-29 22:13:56 +0200

    changed layout name to capital

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "../styles/globals.css";
-import Layout from "@/components/Layout";
+import Layout from "@/components/layout";
 import Navbar from "@/components/Navbar";
 
 function MyApp({ Component, pageProps }) {
